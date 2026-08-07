61a0d0a7ca4d344a72ba65f3fa93b1051589b13e
minviellesebastien-ctrl <minvielle.sebastien@gmail.com>
2026-08-07 23:00:57 +0200

    Add event card HTML structure to script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+document.getElementById("app").innerHTML = `
+<div class="event-card">
+    <div class="countdown">J-21</div>
 
+    <div class="event-info">
+        <div class="type">CONCERT</div>
+
+        <h2>INDOCHINE</h2>
+
+        <div class="date">
+            28 août 2026 • 20:00
+        </div>
+
+        <div class="place">
+            Stade de France<br>
+            Saint-Denis
+        </div>
+    </div>
+</div>
+
+<div class="event-total">
+    <span>27</span>
+    <small>ÉVÉNEMENTS</small>
+</div>
+
+<button class="btn blue">
+    Mes événements
+</button>
+
+<button class="btn orange">
+    Ajouter un événement
+</button>
+`;
