f90f1d30024a62379b364e335b0ef4103d127d73
minviellesebastien-ctrl <minvielle.sebastien@gmail.com>
2026-08-08 14:42:06 +0200

    Add initial HTML structure for events page

---
+++ evenements.js
@@ -0,0 +1,13 @@
+document.getElementById("app").innerHTML = `
+
+<header class="topbar">
+
+    <a href="index.html" class="back">←</a>
+
+    <h1>Mes événements</h1>
+
+</header>
+
+<div id="liste"></div>
+
+`;
