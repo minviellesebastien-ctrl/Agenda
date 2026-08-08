4456023a72cbf89cbb66252f2e8a219ed0faddec
minviellesebastien-ctrl <minvielle.sebastien@gmail.com>
2026-08-08 14:43:42 +0200

    Update evenements.js

--- evenements.js
+++ evenements.js
@@ -11,3 +11,40 @@ document.getElementById("app").innerHTML = `
 <div id="liste"></div>
 
 `;
+
+const liste = document.getElementById("liste");
+
+const events =
+    JSON.parse(localStorage.getItem("agenda-events")) || [];
+
+if(events.length===0){
+
+    liste.innerHTML=`
+        <p class="vide">
+            Aucun événement enregistré.
+        </p>
+    `;
+
+}else{
+
+    events.forEach(event=>{
+
+        liste.innerHTML+=`
+
+        <div class="carte ${event.categorie}">
+
+            <h2>${event.titre}</h2>
+
+            <p>${event.date} • ${event.heure}</p>
+
+            <p>${event.lieu}</p>
+
+            <p>${event.ville}</p>
+
+        </div>
+
+        `;
+
+    });
+
+}
