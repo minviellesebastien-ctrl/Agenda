a81f4cb6f89e8a1b464cc5c0b814c59fac8de9f1
minviellesebastien-ctrl <minvielle.sebastien@gmail.com>
2026-08-09 08:54:19 +0200

    Add details.js for displaying event details

---
+++ details.js
@@ -0,0 +1,33 @@
+document.getElementById("app").innerHTML = `
+
+<header class="topbar">
+
+    <a href="evenements.html" class="back">←</a>
+
+    <h1>Détails</h1>
+
+</header>
+
+<div class="carte">
+
+    <div class="titre" id="titre"></div>
+
+    <div class="ligne" id="date"></div>
+
+    <div class="ligne" id="heure"></div>
+
+    <div class="ligne" id="lieu"></div>
+
+    <div class="adresseTitre">
+        Adresse
+    </div>
+
+    <div class="adresse" id="adresse"></div>
+
+    <div class="bouton" id="btnAdresse">
+        Modifier l'adresse
+    </div>
+
+</div>
+
+`;
